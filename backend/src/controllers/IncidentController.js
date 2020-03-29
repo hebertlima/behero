cce8ad6056c318acb2fc7c3ed031c1fdd733c2c9
Hebert Lima <hebert.dev@hotmail.com>
2020-03-28 21:05:19 -0300

    front web created

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -8,7 +8,7 @@ module.exports = {
 
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-            .limit(5)
+            .limit(5)            
             .offset((page - 1) * 5)
             .select(['incidents.*',
                 'ongs.name',
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    useEffect(() => {
+        api.get('profiles', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            console.log(response);
+
+            setIncidents(response.data);
+        });
+    }, [ongId]);
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incidents => incidents.id !== id));
+        } catch (error) {
+            alert('Erro ao deletar caso');
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logo} alt="Be The Hero" />
+                <span>bem vinda, <strong>{ongName}</strong></span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button" onClick={handleLogout}>
+                    <FiPower size={18} color="#e02041" />
+                </button>
+            </header>
+
+            <h1>Casos Cadastrados</h1>
+            {/* 4e5af3f8 */}
+            <ul>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
+
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.description}</p>
+
+                        <strong>VALOR:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+
+                        <button type="button" onClick={() => handleDeleteIncident(incident.id)}>
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div >
+    );
+}
